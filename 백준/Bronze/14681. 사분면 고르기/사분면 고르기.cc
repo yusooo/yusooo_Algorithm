428d0846d89c1bbeed1b34bb27ebf04d79d4f2cb
YuNahyun <100765406+yusooo@users.noreply.github.com>
2022-09-24 18:29:50 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if(x>0){
+		if(y>0) printf("1");
+		else if(y<0) printf("4");
+	} else{
+		if(y>0) printf("2");
+		else if(y<0) printf("3");
+	}
+	return 0;
+}
